5f136e939ab8f03e325f730f082e2cff4f2d4fa0
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-06-01 14:57:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function tarefa01(){
+   const primeiraTarefa = document.querySelector("#tarefa-01");
+   const inputElement = document.querySelector("#text");
+
+primeiraTarefa.innerHTML = inputElement.value;
+
+}
+
+function tarefa02(){
+    const segundaTarefa = document.querySelector("#tarefa-02");
+    const segInput = document.querySelector("#text2");
+
+    segundaTarefa.innerHTML = segInput.value;
+    
+}
+
+function tarefa03(){
+    const terceiraTarefa = document.querySelector("#tarefa-03");
+    const tercInput = document.querySelector("#text3");
+
+    terceiraTarefa.innerHTML = tercInput.value;
+}
+
+function tarefa04(){
+    const quartTarefa = document.querySelector("#tarefa-04");
+    const quartInput = document.querySelector("#text4");
+
+    quartTarefa.innerHTML = quartInput.value;
+}
+
+function tarefa05(){
+    const quintTarefa = document.querySelector("#tarefa-05");
+    const quintInput = document.querySelector("#text5");
+    
+    quintTarefa.innerHTML = quintInput.value;
+}
